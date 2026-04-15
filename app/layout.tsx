3a9fe884780d818d0eeca65b1618483520299c20
Shreyansh1775 <shreyanshjha170705@gmail.com>
2026-04-15 23:21:10 +0530

    Navbar profile section added.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,13 @@
 import "./globals.css";
-import Navbar from "";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const hideNavbar =
-    typeof window !== "undefined" &&
-    (window.location.pathname === "/login" ||
-      window.location.pathname === "/register");
-
   return (
     <html lang="en">
-      <body className="min-h-screen bg-gray-50">
-        {!hideNavbar && <Navbar />}
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
